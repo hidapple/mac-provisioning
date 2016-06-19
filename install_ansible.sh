a17e17c9573c06ff3f8239bebafb2bf6f03235d5
hidapple <shotfsh@gmail.com>
2016-06-19 18:47:46 +0900

    Some changes

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,16 +3,16 @@ which brew >/dev/null 2>&1
 if [ $? -ne 0 ]
 then
   echo "[INFO] homebrew is not installed."
-  echo "[INFO] start to install homebrew."
+  echo "[INFO] installing homebrew."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  echo "[INFO] update homebrew."
+  echo "[INFO] updating homebrew."
   brew update
 fi
 
 which ansible >/dev/null 2>&1
 if [ $? -ne 0 ]
 then
-  echo "[INFO] start to install ansible."
+  echo "[INFO] installing ansible."
   brew install ansible
 fi
 
