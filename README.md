dfff6c1fbcbf09f37786431816c132e97b55e258
hidapple <shotfsh@gmail.com>
2016-12-04 02:46:17 +0900

    Bug fix

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# macOS Provisioning with Ansible
-Easy provisioning for macOS
+# MacOS provisioning with Ansible
+Local env provisioning for MacOS.
+
+## How to
+Just clone and execute setup.sh!
+
+```shell
+$ sh setup.sh
+```
 
--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 which brew >/dev/null 2>&1
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
   echo "[INFO] homebrew is not installed."
   echo "[INFO] installing homebrew."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -12,8 +11,7 @@ else
 fi
 
 which ansible >/dev/null 2>&1
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
   echo "[INFO] installing ansible."
   brew install ansible
 else
@@ -26,13 +24,24 @@ ansible-playbook -vv -i hosts macOS-playbook.yml -K
 
 ### Prepare .dotfiles
 echo "[INFO] Preparing dotfiles."
-dir=~/.dotfiles
-if [ -d $dir ]
-then
+if [ -d ~/.dotfiles ]; then
   echo "[INFO] .dotfiles already exists."
 else
   echo "[INFO] Creating .dotfiles"
   git clone https://github.com/hidapple/dotfiles.git ~/.dotfiles
   sh ~/.dotfiles/link.sh
+fi
+
+### Create .gitconfig.local
+echo "[INFO] Create .gitconfig.local."
+if [ -f ~/.gitconfig.local ]; then
+  echo "[INFO] .gitconfig.local already exists."
+else
+  echo "name: \c" && read name
+  echo "email: \c" && read email
+  echo "[user]" > ~/.gitconfig.local
+  echo "  name   = $name" >> ~/.gitconfig.local
+  echo "  email  = $email" >> ~/.gitconfig.local
+  echo "[INFO] Created ~/.gitconfig.local."
 fi
 
