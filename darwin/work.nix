9aca4495d5c34a77a39fd090ca812852afab578b
hidapple <shotfsh@gmail.com>
2026-07-19 15:20:18 +0900

    add work tools

--- darwin/work.nix
+++ darwin/work.nix
@@ -2,7 +2,15 @@
 {
   # Work-only packages.
   environment.systemPackages = with pkgs; [
+    # GNU coreutils as g-prefixed commands (gcsplit, gls, ...) like Homebrew.
+    coreutils-prefixed
+    # Expose GNU sed as `gsed` (like Homebrew) so it doesn't shadow BSD sed.
+    (runCommand "gsed" { } ''
+      mkdir -p $out/bin
+      ln -s ${gnused}/bin/sed $out/bin/gsed
+    '')
     kubectl
+    yq-go
     # gcloud's component manager is disabled under Nix; declare components
     # via withExtraComponents instead.
     (google-cloud-sdk.withExtraComponents [
